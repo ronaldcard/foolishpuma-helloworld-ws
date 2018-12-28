05659e7f8ae207b84d568c68a99323d62ad41e3f
tony.card <tcard@pillartechnology.com>
2018-12-28 16:14:33 -0500

    added ApplicationConfiguration, updated tests.

---
+++ src/main/kotlin/com/foolishpuma/helloworld/application/ApplicationConfiguration.kt
@@ -0,0 +1,8 @@
+package com.foolishpuma.helloworld.application
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ApplicationConfiguration {
+
+}
--- src/test/kotlin/com/foolishpuma/helloworld/application/HelloWorldWSApplicationTests.kt
+++ src/test/kotlin/com/foolishpuma/helloworld/application/HelloWorldWSApplicationTests.kt
@@ -1,16 +1,22 @@
 package com.foolishpuma.helloworld.application
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
-@SpringBootTest
+@SpringBootTest(classes = [ApplicationConfiguration::class])
 class HelloWorldWSApplicationTests {
+
+	@Autowired
+	lateinit var applicationConfiguration: ApplicationConfiguration
 
 	@Test
 	fun contextLoads() {
+		assertThat(this.applicationConfiguration).isNotNull
 	}
 
 }
